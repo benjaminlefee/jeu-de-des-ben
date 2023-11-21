0ab62908e0e20a0f6aa957299199c540ad9597af
benjaminlefee <90236512+benjaminlefee@users.noreply.github.com>
2023-11-21 17:37:54 +0100

    ajout fonction hold

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $("#newGame").on("click",function(event){
   $(scorePlayer2).text(0); 
   $(roundPlayer1).text(0);
   $(roundPlayer2).text(0);
-
+  
   let ROUND = 0;
   let score1 = 0; // total score player 1
   let score2 = 0; // total score player 2
@@ -12,26 +12,31 @@ $("#newGame").on("click",function(event){
 
   $("#pointRedplayer1").addClass("active");
   
-    // the players have their points at 0
-    $("scorePlayer1").text(0);
-    $("scorePlayer2").text(0);
-  
-    $("#dice").remove(); // deleted dice image 
-    $("#diceDiv").append(`<img id="dice" src="./images/dice-1.png" alt="dice 1"/>`); 
-    // added image of dice 1 to start the game
-     function changePlayer(){
-      ROUND = 0;
-      $(`#roundPlayer${setPlayer}`).text(`${ROUND}`)
+  // the players have their points at 0
+  $("scorePlayer1").text(0);
+  $("scorePlayer2").text(0);
+
+  $("#dice").remove(); // deleted dice image 
+  $("#diceDiv").append(`<img id="dice" src="./dice/dice-1.png" alt="dice 1"/>`); // added image of dice 1 to start the game
+   function changePlayer(){
+    ROUND = 0;
+    $(`#roundPlayer${setPlayer}`).text(`${ROUND}`)
+    $(`#pointRedplayer${setPlayer}`).removeClass("active");
+
+    if(setPlayer === 1){
+        setPlayer = 2}
+    else{ 
+        setPlayer = 1}; 
+      $(`#pointRedplayer${setPlayer}`).addClass("active");
+   };
+// end of player switch function
+   function winner(){
+      $("#HOLD").append(`<p>player${setPlayer}winner !</p>`);
       $(`#pointRedplayer${setPlayer}`).removeClass("active");
-  
-      if(setPlayer === 1){
-          setPlayer = 2}
-      else{ 
-          setPlayer = 1}; 
-        $(`#pointRedplayer${setPlayer}`).addClass("active");
-     };
-  // end of player switch function
-  // roll dice function 
+      $("#HOLD").off("click",),
+      $("#rollDice").off("click");
+   }
+// roll dice function 
   $("#rollDice").on("click", function(){
    
     let randomNumber = Math.floor(Math.random() * 6) + 1;
@@ -47,4 +52,22 @@ $("#newGame").on("click",function(event){
     }
 // add temporary points to the overall score
   })
-    })
+  $("#HOLD").on("click", function(){
+    if (setPlayer == 1){// if player 1 is active the score goes to him 
+    score1 += ROUND
+    $(`#scorePlayer${setPlayer}`).text(`${score1}`)
+    }
+    else{// If it is player 2, the score goes to player 2
+    score2 += ROUND
+    $(`#scorePlayer${setPlayer}`).text(`${score2}`)
+    };
+    // Declare a player winner if the score equal to or greater than 100
+    if (score1 >= 100 || score2 >= 100){
+    $("#HOLD").on("click", winner())
+    }
+    else{
+    changePlayer(); 
+    }
+  }
+)
+})
