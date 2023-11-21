c4efd7cef5d5da382b205ef4b03967da1f3c14e1
benjaminlefee <90236512+benjaminlefee@users.noreply.github.com>
2023-11-21 08:43:28 +0100

    ajout fonction démarrage du jeu

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
+// start the game
+$("#newGame").on("click",function(event){
+  $(scorePlayer1).text(0);
+  $(scorePlayer2).text(0); 
+  $(roundPlayer1).text(0);
+  $(roundPlayer2).text(0);
 
+  let ROUND = 0;
+  let score1 = 0; // total score player 1
+  let score2 = 0; // total score player 2
+  let setPlayer = 1; // player 1 start
 
-
-
+  $("#pointRedplayer1").addClass("active");
+  
+    // the players have their points at 0
+    $("scorePlayer1").text(0);
+    $("scorePlayer2").text(0);
+  
+    $("#dice").remove(); // deleted dice image 
+    $("#diceDiv").append(`<img id="dice" src="./images/dice-1.png" alt="dice 1"/>`); 
+    // added image of dice 1 to start the game
+     function changePlayer(){
+      ROUND = 0;
+      $(`#roundPlayer${setPlayer}`).text(`${ROUND}`)
+      $(`#pointRedplayer${setPlayer}`).removeClass("active");
+  
+      if(setPlayer === 1){
+          setPlayer = 2}
+      else{ 
+          setPlayer = 1}; 
+        $(`#pointRedplayer${setPlayer}`).addClass("active");
+     };
+  // end of player switch function
+    })
